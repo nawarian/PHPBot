08825586319590fbff0dd6d6dc8b3d102c066bab
nawarian <nickolas@phpsp.org.br>
2015-08-01 13:51:03 -0300

    Fixing some buggy declarations

--- src/PHPBot/Pointer/Command/Factory.php
+++ src/PHPBot/Pointer/Command/Factory.php
@@ -15,23 +15,27 @@ class Factory
         $this->loop = $loop;
     }
 
-    public function moveTo($x, $y, OS\OperationalSystem $os)
+    public function moveTo($x, $y, OS\OperatingSystem $os)
     {
-        return new {$os->toNamespaceString()}\MoveTo($this->loop, $x, $y);
+        $className = "PHPBot\\Pointer\\Command\\{$os->toNamespaceString()}\\MoveTo";
+        return new $className($this->loop, $x, $y);
     }
 
-    public function click($mouseButton, OS\OperationalSystem $os)
+    public function click($mouseButton, OS\OperatingSystem $os)
     {
-        return new {$os->toNamespaceString()}\Click($this->loop, $mouseButton);
+        $className = "PHPBot\\Pointer\\Command\\{$os->toNamespaceString()}\\Click";
+        return new $className($this->loop, $mouseButton);
     }
 
-    public function holdClick($mouseButton, OS\OperationalSystem $os)
+    public function holdClick($mouseButton, OS\OperatingSystem $os)
     {
-        return new {$os->toNamespaceString()}\HoldClick($this->loop, $mouseButton);
+        $className = "PHPBot\\Pointer\\Command\\{$os->toNamespaceString()}\\HoldClick";
+        return new $className($this->loop, $mouseButton);
     }
 
-    public function releaseClick($mouseButton, OS\OperationalSystem $os)
+    public function releaseClick($mouseButton, OS\OperatingSystem $os)
     {
-        return new {$os->toNamespaceString()}\ReleaseClick($this->loop, $mouseButton);
+        $className = "PHPBot\\Pointer\\Command\\{$os->toNamespaceString()}\\ReleaseClick";
+        return new $className($this->loop, $mouseButton);
     }
 }
